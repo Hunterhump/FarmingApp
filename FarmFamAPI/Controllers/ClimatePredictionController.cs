8715156b7e2b05f2122919115fec1125c9ed0fad
Hunterhump <dhh00006@mix.wvu.edu>
2024-03-26 19:03:30 -0400

    Controller

---
+++ FarmFamAPI/Controllers/ClimatePredictionController.cs
@@ -0,0 +1,19 @@
+﻿using FarmFamAPI.Entities;
+using FarmFamAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmFamAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClimatePredictionController : Controller
+    {
+      private readonly IClimatePredictionService climatePredictionService;
+        public ClimatePredictionController(IClimatePredictionService climatePredictionService)
+        {
+            this.climatePredictionService = climatePredictionService;
+        }
+        [HttpGet("getGetClimatePredictionByState")]
+        public async Task<List<ClimatePrediction>> GetClimatePredictionsByState(char State);
+    }
+}
--- FarmFamAPI/Program.cs
+++ FarmFamAPI/Program.cs
@@ -1,6 +1,11 @@
+using FarmFamAPI.Data;
+using FarmFamAPI.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IClimatePredictionService, ClimatePredictionService>();
+builder.Services.AddDbContext<DbContextClass>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
