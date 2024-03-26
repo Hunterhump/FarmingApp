c6184e1ac160eed576bcd953b80f3d28a539dc2d
Hunterhump <dhh00006@mix.wvu.edu>
2024-03-26 14:01:48 -0400

    CLass Commit

---
+++ FarmFamAPI/Entities/ClimatePrediction.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace FarmFamAPI.Entities
+{
+    public class ClimatePrediction
+    {
+        public int ClimatePredictiom { get; set; }
+        public int PredictionID { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public int? Temperature { get; set; } // Nullable int
+        public string Rainfall { get; set; }
+        public DateTime? Date { get; set; } // Nullable DateTime
+    }
+}
+
