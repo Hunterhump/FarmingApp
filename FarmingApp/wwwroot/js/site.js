51a30039447b0ea1aa37989deb7f5b83e90f9647
psm00005 <142524898+psm00005@users.noreply.github.com>
2024-04-10 12:05:36 -0400

    Updating Javascirpt

--- FarmingApp/wwwroot/js/site.js
+++ FarmingApp/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+{
+    async function displayCropCompatablilties() {
+        const response = await fetch('');
+        const data = await response.json();
+        document.getElement
+    }
+}
